33c2685bd16e8610c30951f1d73e1acbbf4e2d01
emjaehyeon-kim/em <emdottami@gmail.com/em>
2014-11-28 19:56:46 +1100

    new article on 2014-11-27

--- _posts/projects/src/knitSrc.R
+++ _posts/projects/src/knitSrc.R
@@ -1,3 +1,16 @@
+#
+# knitPost aims to convert a R markdown (Rmd) file into a markdown file (md) for an article.
+# It assumes the R markdown file should be named as '_YYYY-MM-DD-Article-Title.Rmd'
+# when the article's title is YYYY-MM-DD-Article-Title.
+# For further details, see http://jaehyeon-kim.github.io/intro/2014/11/19/R-Rroject-Setup/
+#
+# Usage
+# source("src/knitSrc.R")
+# knitPost("YYYY-MM-DD-Article-Title")
+#
+# last modified on Nov 20, 2014
+#
+
 knitPost <- function(title, base.url = "../../") {
   require(knitr)
   opts_knit$set(base.url = base.url)
