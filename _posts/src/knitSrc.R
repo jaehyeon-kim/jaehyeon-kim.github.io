379458f6615b322823dc72049f5960c1779160fe
emjaehyeon-kim/em <emdottami@gmail.com/em>
2014-12-06 15:41:18 +1100

    2014-12-06

--- _posts/src/knitSrc.R
+++ _posts/src/knitSrc.R
@@ -14,9 +14,9 @@
 knitPost <- function(title, base.url = "") {
   require(knitr)
   opts_knit$set(base.url = base.url)
-  fig.path <- paste0("../","figs/", sub(".Rmd$", "", basename(title)), "/")
+  fig.path <- paste0("figs/", sub(".Rmd$", "", basename(title)), "/")
   opts_chunk$set(fig.path = fig.path)
   opts_chunk$set(fig.cap = "center")
   render_jekyll()
-  knit(paste0("_",title,".Rmd"), paste0("../",title,".md"), envir = parent.frame())
+  knit(paste0("_",title,".Rmd"), paste0(title,".md"), envir = parent.frame())
 }
